fb023b685c5f79b4031f78ef9987c1ff566bea52
Mariyan Karakiev <mariqnkarakiev@gmail.com>
2022-03-31 01:18:40 +0300

    Usings cleared

--- Streamy/Streamy.Infrastructure/Data/Repositories/IApplicationDbRepository.cs
+++ Streamy/Streamy.Infrastructure/Data/Repositories/IApplicationDbRepository.cs
@@ -1,9 +1,4 @@
 ﻿using Streamy.Infrastructure.Data.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Streamy.Infrastructure.Data.Repositories
 {
