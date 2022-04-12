bb36b78df3f4b16fff8c6b28313a977162924776
Mariyan Karakiev <mariqnkarakiev@gmail.com>
2022-04-12 14:46:48 +0300

    Added properties to Genre view model

---
+++ Streamy/Streamy.Core/Models/GenreListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streamy.Core.Models
+{
+    public class GenreListViewModel
+    {
+        public GenreListViewModel()
+        {
+            Genres = new List<GenreViewModel>();
+        }
+        public List<GenreViewModel> Genres { get; set; }
+    }
+}
--- Streamy/Streamy.Core/Models/GenreViewModel.cs
+++ Streamy/Streamy.Core/Models/GenreViewModel.cs
@@ -5,10 +5,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Streamy.Core.Models
 {
-    public class GenreModel
+    public class GenreViewModel
     {
+        public short Id { get; set; }
+
         [Required]
         [StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
+        public int? Albums { get; set; }
+        public int? Songs { get; set; }
     }
 }
