9e449038cb3baaeb92daede2543970b9d550c3c5
Mariyan Karakiev <mariqnkarakiev@gmail.com>
2022-04-25 15:39:47 +0300

    Implemented ModelBinding for DateTime

---
+++ Streamy/Streamy/ModelBinders/DateTimeModelBinder.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace Streamy.ModelBinders
+{
+    public class DateTimeModelBinder : IModelBinder
+    {
+        private readonly string _customDateFormat;
+
+        public DateTimeModelBinder(string customDateFormat)
+        {
+            _customDateFormat = customDateFormat;
+        }
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult result = bindingContext
+                 .ValueProvider
+                 .GetValue(bindingContext.ModelName);
+
+            if (result != ValueProviderResult.None && !string.IsNullOrEmpty(result.FirstValue))
+            {
+                DateTime parsedValue = DateTime.MinValue;
+                bool success = false;
+                string dateValue = result.FirstValue;
+
+                try
+                {
+                    parsedValue = DateTime.ParseExact(dateValue, _customDateFormat, CultureInfo.InvariantCulture);
+                    success = true;
+                }
+
+                catch (FormatException)
+                {
+                    try
+                    {
+                        parsedValue = DateTime.Parse(dateValue, new CultureInfo("bg-bg"));
+                    }
+                    catch (Exception e)
+                    {
+
+                        bindingContext.ModelState.AddModelError(bindingContext.ModelName, e, bindingContext.ModelMetadata);
+                    }
+                }
+                catch (Exception e)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, e, bindingContext.ModelMetadata);
+                }
+
+                if (success)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(parsedValue);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
